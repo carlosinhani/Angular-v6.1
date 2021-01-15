39ae89d635cae4d5303301ae989c0e40af4e856b
Carlos.Inhani <carlos.inhani@gmail.com>
2021-01-15 09:29:44 -0300

    keyvalue-Pipe

--- src/app/key-value/key-value.component.ts
+++ src/app/key-value/key-value.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeyValueComponent implements OnInit {
 
+  cursos = [
+     { id: '1', nome: 'Angular'},
+     { id: '2', nome: 'React'},
+  ];
+
+  cursosComparador(a: any, b: any) {
+    if (a.key === b.key) {
+      return 0;
+    }
+    return a.key > b.key ? -1 : 1;
+  }
+
   constructor() { }
 
   ngOnInit() {
